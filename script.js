fb5ed01272aafb9d161df561e17e88fb184eebec
gauridahake24 <gauridahake24@gmail.com>
2023-04-14 20:20:39 +0530

    First-commit

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const bar = document.getElementById('bar');
-const nav = document.getElementById('nav')
+const nav = document.getElementById('navbar');
 
 if (bar) {
-    bar,
-    addEventListener('click', () => {
+    bar.addEventListener('click', () => {
         nav.classList.add('active');
+
     })
 }
